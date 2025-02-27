b9259b7483cde0b8c562fee6ac2e8fc31f5151c3
A00325351: Stephen Curran <A00325351@student.tus.ie>
2025-02-27 19:55:30 +0000

    updated spaceprogram to fully working

--- spaceprogram/target/classes/static/js/main.js
+++ spaceprogram/target/classes/static/js/main.js
@@ -5,37 +5,57 @@ $(document).ready(function() {
 	fetchShuttles();
 
 	// Event listener for mission info button clicks
-	$(document).on('click', '.infoButton', function () {
-	    showMissionDetails($(this).data('id'));
+	$(document).on('click', '.infoButton', function() {
+		showMissionDetails($(this).data('id'));
 	});
-   
-	
+
+	// Event listener for orbitor change
+	$(document).on('change', '#orbitors', function() {
+		let orbName = ($('#orbitors option:selected').val());
+		alert(orbName);
+		if (orbName === "All orbitors") {
+			fetchMissions();
+			displayMissions(missions);
+		}
+		else {
+			fetch('http://localhost:8081/missions/search/' + orbName)
+				.then(response => response.json())
+				.then(data => {
+					$('.list_missions').text(`Orbitor: ` + orbName);
+					displayMissions(data);
+				})
+		}
+	});
+
 });
 
 
 
-function fetchShuttles(){
+function fetchShuttles() {
 	$.getJSON('http://localhost:8081/shuttles')
-	.done(populateShuttles)
-	.fail(error => console.error('Error fetching shuttle data.', error));
+		.done(populateShuttles)
+		.fail(error => console.error('Error fetching shuttle data.', error));
 }
 
 // Function to populate existing dropdown 
-function populateShuttles(shuttles)	{
+function populateShuttles(shuttles) {
 	window.allShuttles = shuttles;
-	const orbitorList = [];
-	
-	shuttles.forEach(shuttle => { 
+	const orbitorList = ['All orbitors'];
+
+	shuttles.forEach(shuttle => {
 		orbitorList.push(shuttle.name);
 		console.log(shuttle.name);
-		});
+	});
+
 
 	var orbitors = document.getElementById("orbitors");
-	for(var i = 0; i < orbitorList.length; i++) {
-	    var option = document.createElement('option');
-	    option.text = option.value = orbitorList[i];
-	    orbitors.add(option, 0);
-	}	
+	for (var i = 0; i < orbitorList.length; i++) {
+		var option = document.createElement('option');
+		option.text = option.value = orbitorList[i];
+		orbitors.add(option, 0);
+	}
+
+	orbitorList.sort();
 };
 
 function fetchMissions() {
@@ -47,7 +67,7 @@ function fetchMissions() {
 function displayMissions(missions) {
 	$('.list_missions').empty(); // clears previous content
 
-	$('.list_missions').append(`<div class="row">`); 
+	$('.list_missions').append(`<div class="row">`);
 	missions.forEach(mission => { // each mission item has the same properties
 		$('.row').append(`
 		        <div class="col-sm-6 col-md-4 col-lg-3"> 
@@ -74,10 +94,10 @@ function showMissionDetails(missionId) {
 
 	// add book details to modal
 	$('.modal-title').text(`Mission details for ${mission.name}`);
-	$('#pic').attr('src', `images/${mission.orbitor}`.toLowerCase()+'_thumb.jpg');
+	$('#pic').attr('src', `images/${mission.orbitor}`.toLowerCase() + '_thumb.jpg');
 	$('#launch').val(mission.launch);
 	$('#landing').val(mission.landing);
-	$('#landing_site').val(mission.landingSite);	
+	$('#landing_site').val(mission.landingSite);
 	$('#duration').val(mission.duration);
 	$('#notes').val(mission.notes);
 
@@ -85,6 +105,4 @@ function showMissionDetails(missionId) {
 	$('#myModal').modal('show');
 }
 
-
-    
 
